ee183d433044e1922fdc7edc4f5ecf62c8f3617e
Jerome Stonebridge <jeromes@jtop.srandd.com>
2017-11-21 21:41:01 -0800

    use gitk or diff

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+#FIXME first check if .bash_profile exists.. (seems to error out if it does not exist) 
 mv ~/.bash_profile ~/.old_bash_profile
+#TODO make this work from the root of the install script, so it dosen't matter where the script is called from
 cp ./bash_profile ~/.bash_profile
 source ~/.bash_profile
 cp ./vimrc ~/.vimrc
 cp -r ./vim ~/.vim
 
- 
 #FIXME
 #cd ~/.vim/bundle/omnisharp-vim
 #git submodule update --init --recursive
@@ -15,7 +16,6 @@ cp -r ./vim ~/.vim
 #cd ~/.vim/bundle/youcompleteme
 #git submodule update --init --recursive
 #py.exe -3 ./install.py --clang-completer
-#'
 
 cd ~/
 
