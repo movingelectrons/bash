3c9ae7168213f5e9ef3dbcf6f3641f359991e350
Jerome Stonebridge <jeromestonebridge@gmail.com>
2018-09-21 12:44:00 -0700

    -

--- install.sh
+++ install.sh
@@ -5,15 +5,13 @@
 profile_file=".bashrc"
 initial_pwd=`pwd`
 
-echo "build-essential cmake python3-dev mono-devel npm git vim-nox vim" #python-dev
+echo "build-essential cmake python3-dev python-dev mono-devel npm git vim-nox vim" #python-dev
 read -r -p "Are the above listed packages installed? [y/N] " response
 response=${response,,}    # tolower
 if [[ "$response" =~ ^(yes|y)$ ]]
 then
     mkdir ~/.vim
     cp -a ./vim/* ~/.vim
-    #cd ~/.vim/bundle/
-    #git clone https://github.com/Valloric/YouCompleteMe.git #TODO use git submodules... < these are rather confusing ATM..
     cd ~/.vim/bundle/YouCompleteMe
     ./install.py --clang-completer --cs-completer --js-completer
     [ -e ~/.bash_profile ] && mv ~/.bash_profile ~/.old_bash_profile|echo "backing up existing .bash_profile to .old_bash_profile..."
